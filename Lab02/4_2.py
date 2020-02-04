eb378a50b20230085508067782cec29037546ba2
Nik Dorndorf <nik.dorndorf@rwth-aachen.de>
2020-02-04 18:59:30 +0100

    Lab02 implemented until 4.2

---
+++ Lab02/4_2.py
@@ -0,0 +1,45 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import learning_functions
+
+
+x_train = np.array([[0.4000, 0.4439],
+                [0.2439, 0.1463],
+                [0.1707, 0.2293],
+                [0.2293, 0.7610],
+                [0.5171, 0.9414],
+                [0.8732, 0.6536],
+                [0.6878, 0.5219],
+                [0.8488, 0.3609],
+                [0.6683, 0.2536],
+                [0.6195, 0.2634]])
+
+
+num_nodes = 10
+
+means = np.random.uniform(size=(num_nodes, x_train.shape[1]))
+vars = np.ones(means.shape[0]) * 1.
+weights = np.ones((means.shape[0], 1))
+
+lr = 0.1
+first_rbf = learning_functions.GaussianRBF(means, vars, weights, lr)
+
+for i in range(20):
+    num_neigh = int(4 - 4/19 * i)
+    first_rbf.som_circle_neighbor_algorithm(x_train, num_neigh)
+
+output = first_rbf.winning_index(x_train)
+
+print(output)
+
+sort_index = np.argsort(output)
+
+sorted_coords = x_train[sort_index]
+
+for co in range(sorted_coords.shape[0]-1):
+    plt.plot(sorted_coords[co:co+2, 0], sorted_coords[co:co+2, 1], 'ro-')
+
+plt.plot([sorted_coords[-1, 0], sorted_coords[0, 0]], [sorted_coords[-1, 1], sorted_coords[0, 1]], 'ro-')
+
+plt.show()
--- Lab02/learning_functions.py
+++ Lab02/learning_functions.py
@@ -2,33 +2,34 @@ import numpy as np
 
 
 def phi_gaussian(x, my, sigma):
-    return np.exp(-np.square(x-my) / (2*np.square(sigma)))
+    r = np.linalg.norm(x-my, 2, axis=-1)
+    return np.exp(-np.square(r) / (2*np.square(sigma)))
 
 
 class GaussianRBF:
 
     def __init__(self, means, vars, weights, lr=0.01, som_lr=0.2):
-        self.n = len(weights)
-        self.means = means
-        self.vars = vars
-        self.weights = weights
+        self.n = weights.shape[0]  # weights shape is (number of nodes * output size)
+        self.means = means  # means shape is (number of nodes * input size)
+        self.vars = vars  # var shape is (number of nodes)
+        self.weights = weights # weights shape is (number of nodes * output size)
         self.lr = lr
         self.som_lr = som_lr
+        self.input_dim = means.shape[1]
+        self.output_dim = weights.shape[1]
 
     def set_learning_rate(self, lr, som_lr=0.2):
         self.lr = lr
         self.som_lr = som_lr
 
     def least_squares(self, x, y):
-        phi_matrix = np.empty((len(x), self.n))
+        phi_matrix = np.empty((x.shape[0], self.n))
         for i in range(phi_matrix.shape[0]):
             for j in range(phi_matrix.shape[1]):
                 phi_matrix[i, j] = phi_gaussian(x[i], self.means[j], self.vars[j])
-
-        #rhs = np.matmul(np.transpose(phi_matrix), y)
-        #lhs = np.matmul(np.transpose(phi_matrix), phi_matrix)
-        w = np.matmul(np.linalg.pinv(phi_matrix), y)
-        self.weights = w
+        for dim_i in range(self.output_dim):
+            w = np.matmul(np.linalg.pinv(phi_matrix), y[:, dim_i])
+            self.weights[:, dim_i] = w
 
     def delta_rule(self, x, y):
         # shuffle data
@@ -36,44 +37,95 @@ class GaussianRBF:
         np.random.shuffle(indices)
         x = x[indices]
         y = y[indices]
-        for i, xk in enumerate(x):
-            phi_k = phi_gaussian(xk, self.means, self.vars)
+        for i in range(x.shape[0]):
+            phi_k = phi_gaussian(np.repeat(x[i], self.means.shape[0], axis=0).reshape(self.means.shape), self.means, self.vars)
             err = y[i] - np.matmul(np.transpose(phi_k), self.weights)
             delta_w = self.lr * err * phi_k
-            self.weights += delta_w
+            self.weights += delta_w.reshape(self.weights.shape)
 
     def forward_pass(self, x):
-        phi_matrix = np.empty((len(x), self.n))
+        phi_matrix = np.empty((x.shape[0], self.n))
         for i in range(phi_matrix.shape[0]):
             for j in range(phi_matrix.shape[1]):
                 phi_matrix[i, j] = phi_gaussian(x[i], self.means[j], self.vars[j])
         return np.matmul(phi_matrix, self.weights)
 
     def res_error(self, x, y):
-        phi_matrix = np.empty((len(x), self.n))
-        for i in range(phi_matrix.shape[0]):
-            for j in range(phi_matrix.shape[1]):
-                phi_matrix[i, j] = phi_gaussian(x[i], self.means[j], self.vars[j])
-        y_pred = np.matmul(phi_matrix, self.weights)
+        y_pred = self.forward_pass(x)
         return np.linalg.norm(y_pred - y, 2)
 
     def abs_res_error(self, x, y):
-        phi_matrix = np.empty((len(x), self.n))
-        for i in range(phi_matrix.shape[0]):
-            for j in range(phi_matrix.shape[1]):
-                phi_matrix[i, j] = phi_gaussian(x[i], self.means[j], self.vars[j])
-        y_pred = np.matmul(phi_matrix, self.weights)
+        y_pred = self.forward_pass(x)
         return np.sum(np.abs(y_pred - y))
 
     def som_algorithm(self, x):
         # shuffle data
         indices = np.arange(x.shape[0])
         np.random.shuffle(indices)
-        x = x[indices]
-        for i, xk in enumerate(x):
-            dk = np.abs(xk-self.means)
+        x = x[indices, :]
+        for i in range(x.shape[0]):
+            dk = np.linalg.norm(x[i]-self.means, 2, axis=1)
             min_index = np.argmin(dk)
-            self.means[min_index] += self.som_lr * (xk-self.means)[min_index]
+            self.means[min_index, :] += self.som_lr * (x[i]-self.means[min_index])
 
+    def som_neighbor_algorithm(self, x, num_neigh=2):
+        # shuffle data
+        indices = np.arange(x.shape[0])
+        np.random.shuffle(indices)
+        x = x[indices, :]
+        for i in range(x.shape[0]):
+            dk = np.linalg.norm(x[i]-self.means, 2, axis=1)
+            min_index = np.argsort(dk)[:num_neigh]
+            self.means[min_index, :] += self.som_lr * (
+                    np.tile(x[i], num_neigh).reshape((-1, x.shape[1])) - self.means[min_index, :])
 
+    def winning_index(self, x):
+        output = []
+        for i in range(x.shape[0]):
+            dk = np.linalg.norm(x[i] - self.means, 1, axis=1)
+            min_index = np.argmin(dk)
+            output.append(min_index)
+        return np.array(output)
 
+    def som_index_neighbor_algorithm(self, x, num_neigh=2):
+        # shuffle data
+        indices = np.arange(x.shape[0])
+        np.random.shuffle(indices)
+        x = x[indices, :]
+        half_neigh = int(num_neigh/2)
+        for i in range(x.shape[0]):
+            dk = np.linalg.norm(x[i]-self.means, 1, axis=1)
+            min_index = np.argmin(dk)
+            half_min = min_index - half_neigh
+            half_max = min_index + half_neigh + 1
+            if half_min < 0:
+                half_min = int(0)
+                half_max = min_index + (min_index - half_min)
+            if half_max >= self.means.shape[0]:
+                half_max = int(self.means.shape[0])
+                half_min = min_index + (half_max - min_index)
+            act_num_neigh = half_max - half_min
+            self.means[half_min:half_max, :] += self.som_lr * (
+                    np.tile(x[i], act_num_neigh).reshape((-1, x.shape[1])) - self.means[half_min:half_max, :])
+
+    def som_circle_neighbor_algorithm(self, x, num_neigh=2):
+        # shuffle data
+        indices = np.arange(x.shape[0])
+        np.random.shuffle(indices)
+        x = x[indices, :]
+        half_neigh = int(num_neigh/2)
+        array_length = x.shape[0]
+        for i in range(x.shape[0]):
+            dk = np.linalg.norm(x[i]-self.means, 1, axis=1)
+            min_index = np.argmin(dk)
+            half_min = min_index - half_neigh
+            half_max = min_index + half_neigh + 1
+            index_array = np.arange(half_min, half_max, 1).astype(int)
+            for curr_index in index_array:
+                if curr_index >= array_length:
+                    temp_index = curr_index - array_length
+                    self.means[temp_index, :] += self.som_lr * (
+                            x[i] - self.means[temp_index, :])
+                else:
+                    self.means[curr_index, :] += self.som_lr * (
+                            x[i] - self.means[curr_index, :])
