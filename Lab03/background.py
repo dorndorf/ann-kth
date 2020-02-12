48db2da0e643cd76e4f5b2a9653f885d2cf1215d
lingran13 <56674655+lingran13@users.noreply.github.com>
2020-02-12 21:29:50 +0100

    Update background.py

--- Lab03/background.py
+++ Lab03/background.py
@@ -16,6 +16,14 @@ x3 = np.array([-1,  1,  1, -1, -1,  1, -1,  1])
 
 xd = np.concatenate((x1, x2, x3)).reshape(-1, len(x1))
 
+#xd may be wrong. 
+'''
+x1d = np.array([1, -1,  1, -1,  1, -1, -1,  1])
+x2d = np.array([1, 1, -1, -1, -1,  1, -1, -1])
+x3d = np.array([1,  1,  1, -1, 1,  1, -1,  1])
+
+xd = np.concatenate((x1d, x2d, x3d)).reshape(-1, len(x1))
+'''
 out = hopf1.asynchr_update(xd)
 
 print(out)
